6442bbf0ec69e128dff89148d701d0d65705c16e
Hunter Stern <vonrosen2000@yahoo.com>
2020-02-09 19:56:07 -0800

    more changes

--- src/main/java/org/hunter/userrequestlogeventingservice/service/RabbitMqService.java
+++ src/main/java/org/hunter/userrequestlogeventingservice/service/RabbitMqService.java
@@ -2,8 +2,12 @@ package org.hunter.userrequestlogeventingservice.service;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
 import java.util.Map;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.hunter.model.UserView;
 import org.hunter.userrequestlogeventingservice.config.AppProperties;
 import org.hunter.userrequestlogeventingservice.config.RabbitConfig;
@@ -14,6 +18,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -29,35 +34,58 @@ public class RabbitMqService {
     @Autowired
     private AppProperties appProperties;
     @Autowired
-    private RabbitTemplate rabbitTemplate;    
-    private RestTemplate restTemplate = new RestTemplate();
+    private RabbitTemplate rabbitTemplate;
+    private RestTemplate restTemplate;
+    private Object restTemplateMutex = new Object();
+
+    public void configureRestTemplate() {
+        if (restTemplate == null) {
+            synchronized (restTemplateMutex) {
+                if (restTemplate == null) {
+                    restTemplate = new RestTemplate();
+                    restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory() {
+                        @Override
+                        protected void prepareConnection(HttpURLConnection connection, String httpMethod)
+                                throws IOException {
+                            if (connection instanceof HttpsURLConnection) {
+                                ((HttpsURLConnection) connection).setHostnameVerifier(new NoopHostnameVerifier());
+                            }
+                            super.prepareConnection(connection, httpMethod);
+                        }
+                    });
+                }
+            }
+        }
+    }
 
     @RabbitListener(queues = RabbitConfig.QUEUE)
-    public void consume(UserRequestLogView userRequestLogView, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag)
-            throws IOException {
-
+    public void consume(UserRequestLogView userRequestLogView, Channel channel,
+            @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         try {
-            if (userRequestLogView == null || userRequestLogView.getUserId() == null || userRequestLogView.getMaxPaymentAmount() == null) {
+            if (userRequestLogView == null || userRequestLogView.getUserId() == null
+                    || userRequestLogView.getMaxPaymentAmount() == null) {
                 channel.basicReject(tag, false);
                 return;
             }
+            configureRestTemplate();
             restTemplate.getForObject(
                     appProperties.getUserServiceBaseUrl() + "/" + userRequestLogView.getUserId().toString(),
                     UserView.class);
             UserRequestLog userRequestLogForSave = new UserRequestLog(null, userRequestLogView.getUserId(),
-            		userRequestLogView.getMaxPaymentAmount().multiply(BigDecimal.valueOf(100)).toBigInteger());
+                    userRequestLogView.getMaxPaymentAmount().multiply(BigDecimal.valueOf(100)).toBigInteger());
             userRequestLogRepository.save(userRequestLogForSave);
             channel.basicAck(tag, false);
         }
         catch (Exception exc) {
-        		System.out.println("Exception processing message from queue " + RabbitConfig.QUEUE + " : " + exc.getMessage());
+            System.out.println(
+                    "Exception processing message from queue " + RabbitConfig.QUEUE + " : " + exc.getMessage());
             channel.basicReject(tag, false);
         }
     }
-    
+
     @RabbitListener(queues = RabbitConfig.DEAD_LETTER_QUEUE)
     public void rePublish(Message failedMessage, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag)
-    			throws IOException {
+            throws IOException {
         Map<String, Object> headers = failedMessage.getMessageProperties().getHeaders();
         Integer retriesHeader = (Integer) headers.get(RabbitConfig.X_RETRIES_HEADER);
         if (retriesHeader == null) {
@@ -69,11 +97,11 @@ public class RabbitMqService {
             rabbitTemplate.send(RabbitConfig.DELAY_EXCHANGE, RabbitConfig.QUEUE, failedMessage);
         }
         else {
-        		System.out.println("Parking message: " + failedMessage);
+            System.out.println("Parking message: " + failedMessage);
             rabbitTemplate.send(RabbitConfig.PARKING_LOT_QUEUE, failedMessage);
         }
-        
+
         channel.basicReject(tag, false);
-    }    
+    }
 
 }
